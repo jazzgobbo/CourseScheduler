c2a6c5645fb53ec1e83155722d3f2fe91acc1a7e
jazzgobbo <jazzgobbo@gmail.com>
2022-11-09 15:19:40 -0600

    cypress

--- cypress/e2e/App.spec.cy.js
+++ cypress/e2e/App.spec.cy.js
@@ -3,7 +3,22 @@
 describe ('Test App', () => {
 
   it ('launches', () => {
-    cy.visit ("http://localhost:3000/");
+    cy.visit("http://localhost:3000/");
   });
+
+  it ('Spring courses shown when Spring selected', () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy=Spring]').click();
+    cy.get('[data-cy=course]').should('contain', 'Spring');
+  });
+
+  it ('Winter courses shown when Spring selected', () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy=Winter]').click();
+    cy.get('[data-cy=course]').should('contain', 'Winter');
+
+  });
+
+
 
 });
